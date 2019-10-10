28e3717987ea256dd5d593dd7e81a7c3876923dd
usmanahmed71 <56296626+usmanahmed71@users.noreply.github.com>
2019-10-10 19:57:14 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+ exports.helloWorld = functions.https.onRequest((request, response) => {
+
+        response.send("Hello from Firebase!");
+
+ });
+ 
+ exports.helloWorldTwo = functions.https.onRequest((request, response) => {
+     
+        response.send("Hello from 22 Firebase!");
+   
+});
+
+exports.helloWorldThree = functions.https.onRequest((request, response) => {
+     
+        response.send("Hello from 33 Firebase!");
+       
+});
